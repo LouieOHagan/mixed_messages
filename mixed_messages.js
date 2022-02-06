21681351a42af5ebd4ec0c64258478c6bf175b7d
Louie O Hagan <louieohagan101@gmail.com>
2022-02-06 23:44:37 +0000

    Created function to randomly generate number that is used as an index to get a random element in an array

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,3 +1,13 @@
 const randomMessages = require('./messagesStored.json');
 
-console.log(randomMessages.person[0]);
+const generateRandomNumber = arr => {
+    return Math.floor(Math.random() * arr.length);
+}
+
+const randomPerson = generateRandomNumber(randomMessages.person);
+const randomPrediction = generateRandomNumber(randomMessages.prediction);
+const randomTime = generateRandomNumber(randomMessages.when);
+
+console.log(randomMessages.person[randomPerson]);
+console.log(randomMessages.prediction[randomPrediction]);
+console.log(randomMessages.when[randomTime]);
